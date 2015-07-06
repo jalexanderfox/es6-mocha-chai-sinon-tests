fbdad1c44aedb212f1363e4004499afe84340f16
jalexanderfox <jalexanderfox@gmail.com>
2015-07-06 12:04:30 -0400

    add arrow functions tests

---
+++ test/test__functions_arrow.js
@@ -0,0 +1,38 @@
+// Javascript Array methods tests including ES6
+expect = require('chai').expect;
+
+//Arrow functions
+describe('arrow functions', function() {
+
+
+	// this doesn't seem to work right now in node --harmony...
+	it.skip('lexical this', function(done){
+
+		this.timeout(500);
+		function Person(){
+			this.age = 0;
+			this.timedout = false;
+			setTimeout(() => {
+				this.timedout = true;
+		    	this.age++; // |this| properly refers to the person object
+		    // console.log('!!!!!!!!! - - - - - !!!!!!!!!', this.age);
+		}, 200);
+		}
+
+		var p = new Person();
+
+		setTimeout(function(){
+			expect(p.age).to.equal(1);
+			expect(p.timedout).to.be.true;
+			done();
+		}, 400);
+
+	});
+}
+
+);
+
+
+// Lexical this
+
+
