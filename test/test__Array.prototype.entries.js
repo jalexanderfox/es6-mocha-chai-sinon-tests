07b8a73db75061ed4715d9919f72e8221b9c6654
jalexanderfox <jalexanderfox@gmail.com>
2015-07-15 10:49:09 -0400

    add test__Array.prototype.entrie

---
+++ test/test__Array.prototype.entries.js
@@ -0,0 +1,21 @@
+// Javascript Array methods tests including ES6
+expect = require('chai').expect;
+
+var arr = ['a', 'b', 'c'];
+var eArr = arr.entries();
+
+//The entries() method returns a new Array Iterator object that contains the key/value pairs for each index in the array.
+describe('Array', function() {
+	describe('#prototype', function(){
+		describe('#entries', function(){
+			it('returns a new Array Iterator object that contains the key/value pairs for each index in the array', function(){
+				expect( eArr.next().value ).to.eql([0, 'a']); // [0, 'a']
+				expect( eArr.next().value ).to.eql([1, 'b']); // [1, 'b']
+				expect( eArr.next().value ).to.eql([2, 'c']); // [2, 'c']
+			});
+		})
+
+	})
+}
+
+);
