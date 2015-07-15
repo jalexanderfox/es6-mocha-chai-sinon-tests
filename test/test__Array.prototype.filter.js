dd475e15c6c256b364fb64093953ee4ae3d97fb3
jalexanderfox <jalexanderfox@gmail.com>
2015-07-15 14:09:11 -0400

    add test__Array.prototype.filter.js

---
+++ test/test__Array.prototype.filter.js
@@ -0,0 +1,40 @@
+// Javascript Array methods tests including ES6
+expect = require('chai').expect;
+
+var arr = [
+  { id: 15 },
+  { id: -1 },
+  { id: 0 },
+  { id: 3 },
+  { id: 12.2 },
+  { },
+  { id: null },
+  { id: NaN },
+  { id: 'undefined' }
+];
+
+function filterByID(obj) {
+  if ('id' in obj && typeof(obj.id) === 'number' && !isNaN(obj.id)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var arrByID = arr.filter(filterByID);
+
+
+//The filter() method creates a new array with all elements that pass the test implemented by the provided function.
+describe('Array', function() {
+	describe('#prototype', function(){
+		describe('#filter', function(){
+			it('creates a new array with all elements that pass the test implemented by the provided function', function(){
+				expect( arr ).to.not.equal( arrByID );
+				expect( arrByID ).to.eql( [{ id: 15 }, { id: -1 }, { id: 0 }, { id: 3 }, { id: 12.2 }] );
+			});
+		})
+
+	})
+}
+
+);
