91eb1f2e4349ae9a781cd81ef4a8e2e137ae982a
jalexanderfox <jalexanderfox@gmail.com>
2015-07-18 14:52:57 -0400

    update polyfills.js, curry the getPolyfill method, comment iife

--- polyfills/polyfills.js
+++ polyfills/polyfills.js
@@ -1,11 +1,27 @@
 
-(function(dirname) {
-	exports.getPolyfill = function(name) {
+
+// the iife way
+// (function(dirname) {
+// 	exports.getPolyfill = function(name) {
+// 		if(process.env.NOPOLYFILLS && process.env.NOPOLYFILLS === 'true'){
+// 			console.log('polyfills are turned off.');
+// 			return this;
+// 		} else {
+// 			return require( dirname + '/' + name);
+// 		}
+// 	}
+// })(__dirname);
+
+// the curry way
+var getPolyfillFrom = function(dirname) {
+	return function(name) {
 		if(process.env.NOPOLYFILLS && process.env.NOPOLYFILLS === 'true'){
 			console.log('polyfills are turned off.');
 			return this;
 		} else {
 			return require( dirname + '/' + name);
 		}
-	}
-})(__dirname);
+	};
+};
+
+exports.getPolyfill = getPolyfillFrom(__dirname);
