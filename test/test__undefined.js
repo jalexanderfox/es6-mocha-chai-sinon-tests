18b8b0f732024e4af54edfbb4b069feab71a46c2
jalexanderfox <jalexanderfox@gmail.com>
2015-07-27 00:07:35 -0400

    add test for global undefined property

---
+++ test/test__undefined.js
@@ -0,0 +1,13 @@
+var expect = require('chai').expect;
+
+// undefined
+// The global undefined property represents the primitive value undefined. It is one of JavaScript's primitive types.
+
+
+describe('undefined', function(){
+	it('represents the primitive value undefined', function(){
+		expect( undefined ).to.be.undefined;
+	});
+});
+
+
